9af7f666a14810729144ed179fd8b27a67e8c476
ethanbg2 <ethanbg2@illinois.edu>
2022-10-03 23:50:05 -0500

    updated structure

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, send_from_directory
+
+app = Flask(__name__,static_folder="build/",static_url_path='')
+
+@app.route("/")
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route("/test")
+def test():
+    return {"test": ["test1", "test2", "test3"]}
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
