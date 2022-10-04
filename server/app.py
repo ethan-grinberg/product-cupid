cd79720e3d61e6315e8233d7607a460508925a58
ethanbg2 <ethanbg2@illinois.edu>
2022-10-03 20:30:49 -0500

    updated back end

--- server/app.py
+++ server/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__,static_folder='client/build',static_url_path='')
 
 @app.route("/")
 def index():
-    return "hello world!"
-    
+    return send_from_directory(app.static_folder, 'index.html')
+
 @app.route("/test")
 def test():
     return {"test": ["test1", "test2", "test3"]}
