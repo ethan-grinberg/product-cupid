b50c91f85bd3daad3e6b2d14e3994db017cc4e76
ethanbg2 <ethanbg2@illinois.edu>
2022-10-03 21:05:04 -0500

    fixed static folder path error

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__,static_folder='client/build',static_url_path='')
+app = Flask(__name__,static_folder='../client/build',static_url_path='')
 
 @app.route("/")
 def index():
