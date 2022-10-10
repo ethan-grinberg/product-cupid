88703788faa956fd906a741011fbe74db157fc94
ethanbg2 <ethanbg2@illinois.edu>
2022-10-10 15:03:22 -0500

    made it so text fits within given box

--- src/Product.js
+++ src/Product.js
@@ -6,24 +6,31 @@ import Typography from '@mui/material/Typography';
 import {CardActionArea } from '@mui/material';
 import Rating from '@mui/material/Rating';
 import React, {useState} from 'react';
+import Box from '@mui/material/Box';
 
 class Product extends React.Component {
    render() {
     return (        
-        <Card sx={{maxwidth: 'md', height: '60vh', display:'flex'}}>
+        <Card sx={{height: '60vh', display:'flex'}}>
         <CardActionArea onClick={event => this.props.handleClick(this.props.product.ProductId, this.props.order)}>
           <CardMedia
             component="img"
-            height="400"
-            maxwidth= 'md'
             image= {this.props.product.ImageURL}
-            sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
+            sx={{height:'35vh', padding: "1em 1em 0 1em", objectFit: "contain" }}
           />
-          <CardContent>
+          <CardContent >
             <Typography gutterBottom variant="h5" component="div">
               ${this.props.product.price}.00
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography
+              sx={{
+                overflow: "hidden",
+                textOverflow: "ellipsis",
+                maxWidth: '200px',
+                display: "-webkit-box",
+                WebkitLineClamp: "2",
+                WebkitBoxOrient: "vertical",
+              }}>
               {this.props.product.ProductTitle}
             </Typography>
             <Rating value={this.props.product.reviews ?? 2} precision={1} readOnly />
